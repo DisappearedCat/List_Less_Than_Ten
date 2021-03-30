c7cb927d58a404583298437696dd604efea6146f
Nail Minnigalin <nail160306@gmail.com>
2021-03-30 20:04:42 +0300

    Complete extra3

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ def less_than_ten(list_):
     return n_list
 
 
+def less_than_x(list_, x):
+    n_list = []
+    for i in list_:
+        if i < x:
+            n_list.append(i)
+
+    return n_list
+
+
 if __name__ == '__main__':
     list_ = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-    list_ = less_than_ten(list_)
+    less = int(input("Enter number: "))
+    list_ = less_than_x(list_, less)
 
     print(list_)
--- tests.py
+++ tests.py
@@ -9,6 +9,11 @@ class Test(unittest.TestCase):
         self.assertEqual(main.less_than_ten([]), [])
         self.assertEqual(main.less_than_ten([11, 12, 13, 14]), [])
 
+    def test_less_than_x(self):
+        self.assertEqual(main.less_than_x([1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89], 22), [1, 1, 2, 3, 5, 8, 13, 21])
+        self.assertEqual(main.less_than_x([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12], 3), [1, 2])
+        self.assertEqual(main.less_than_x([], 5), [])
+        self.assertEqual(main.less_than_x([11, 12, 13, 14], 40), [11, 12, 13, 14])
 
 if __name__ == '__main__':
     unittest.main()
