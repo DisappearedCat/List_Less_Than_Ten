57d3cf27803b06290521034ab8707a1d6d7b90a5
Nail Minnigalin <nail160306@gmail.com>
2021-03-30 19:57:41 +0300

    Complete main task

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
-
-def less_than_ten(list):
+def less_than_ten(list_):
     n_list = []
-    for i in list:
+    for i in list_:
         if i < 10:
             n_list.append(i)
 
     return n_list
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+if __name__ == '__main__':
+    list_ = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+    list_ = less_than_ten(list_)
+
+    print(list_)
